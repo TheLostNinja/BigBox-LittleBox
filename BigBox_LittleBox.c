b1b2c37cfe029ee46c62bb93144190c7a845ab99
TheLostNinja <116140256+TheLostNinja@users.noreply.github.com>
2025-10-19 09:12:28 +0300

    Update BigBox_LittleBox.c

--- BigBox_LittleBox.c
+++ BigBox_LittleBox.c
@@ -8,7 +8,7 @@ enum 			TargetFormat targetFormat;
 FILE			*source_file, *tilefile1, *tilefile2, *tilemapfile, *palfile;
 long			file_size,tx,tile_x;
 int				colNum,pix_loc,img_width,img_height,ty,tile_y;
-short			pal_loc,img_depth,tile_depth,tile_size,coef,pix0,pix1;
+short			pal_loc,img_depth,tile_depth,tile_size,coef,pix;
 bool			full_size,ref,isTileMap;
 
 enum SourceFormat
@@ -265,8 +265,8 @@ int get_tile_el_value(short x,short y)
  if(sourceFormat==FORMAT_BMP)					return bytStr[file_size-(((tile_y*tile_size+y)*img_width)/coef+((img_width-tile_x*tile_size)/coef-x))];
  else if(sourceFormat==FORMAT_ROHGA_DECR)		return bytStr[(file_size/2)*((x%4)/2)+tile_x*16+(x%2)+y*2];
  else if(sourceFormat==FORMAT_PCE_CG)			return bytStr[16*((x%4)/2)+tile_x*32+(x%2)+y*2];
- else if(sourceFormat==FORMAT_PLANAR4_16x16)	return bytStr[(targetFormat==TARGET_NEOGEO_SPR?tile_x:tile_x/4)*128+(full_size==true?(x%2):(((targetFormat==TARGET_NEOGEO_SPR?x:tile_x)%2)*64+(((targetFormat==TARGET_NEOGEO_SPR?x:tile_x)%4)/2)*32))+(targetFormat==TARGET_NEOGEO_SPR?(x/2)*2:x)+((y*4)<<full_size)];
- else if(sourceFormat==FORMAT_NEO_MIRROR)		return bytStr[tile_x*128+((x/4)%2)*64+(x%4)+(y*4)];
+ else if(sourceFormat==FORMAT_PLANAR4_16x16)	return bytStr[(targetFormat==TARGET_NEOGEO_SPR?tile_x:tile_x/4)*128+(full_size?x/4:(targetFormat==TARGET_NEOGEO_SPR?(x/4):tile_x%2)*64)+(targetFormat==TARGET_NEOGEO_SPR?0:((tile_x%4)/2)*32)+(((x%3)+y*4)<<full_size)];
+ else if(sourceFormat==FORMAT_NEO_MIRROR)		return bytStr[tile_x*128+(x/4)*64+(x%4)+(y*4)];
  else if(sourceFormat==FORMAT_OLD_SPRITE)		return bytStr[(tile_x/16)*1024+(tile_x%4)*8+((tile_x%16)/4)*256+(x/4)*4+x/2+y*32];
  else if(sourceFormat==FORMAT_TAITO_Z)			return bytStr[(targetFormat==TARGET_NEOGEO_SPR?tile_x:tile_x/2)*64+(ref==true?1-((targetFormat==TARGET_NEOGEO_SPR?x:tile_x)%2):(targetFormat==TARGET_NEOGEO_SPR?x:tile_x)%2)+(3-x)*2+y*8];
  else if(sourceFormat==FORMAT_UNDERFIRE)		return bytStr[(tile_x/4)*160+(1-(tile_x%2))*5+((tile_x%4)/2)*80+x+y*10];
