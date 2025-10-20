0848d0d1adece5cbd6a477c79470a8e3a97146fa
TheLostNinja <116140256+TheLostNinja@users.noreply.github.com>
2025-10-20 06:30:30 +0300

    Update BigBox_LittleBox.c

--- BigBox_LittleBox.c
+++ BigBox_LittleBox.c
@@ -632,9 +632,9 @@ int main(int argc,char *argv[])
       {
        for(z=0;z<depth;z++)
        {
-        if(targetFormat==TARGET_OLD_SPRITE)			tiles[(tile_y*tiles_y+tile_x)*1024+y*32+(x/4)*4+(7-z)/2]|=((((pix&(1<<z)))>>z)<<(3-(x%4)))<<((1-(z%2))*4);
+        if(targetFormat==TARGET_OLD_SPRITE)			tiles[(isTileMap?tile_y*tiles_y+tile_x:unique_tiles)*1024+y*32+(x/4)*4+(7-z)/2]|=((((pix&(1<<z)))>>z)<<(3-(x%4)))<<((1-(z%2))*4);
         else if(targetFormat==TARGET_NEOGEO_SPR)	tiles[tile_x*128+(x/8)*64+y*4+z]|=((pix&(1<<z))>>z)<<(x%8);
-        else if(targetFormat==TARGET_TC0180VCU)		tiles[(tile_y*tiles_y+tile_x)*(tile_size*tile_size*depth/8)+y*tile_size+z]|=((pix&(1<<z))>>z)<<(7-(x%8));
+        else if(targetFormat==TARGET_TC0180VCU)		tiles[(isTileMap?tile_y*tiles_y+tile_x:unique_tiles)*(tile_size*tile_size*depth/8)+y*tile_size+z]|=((pix&(1<<z))>>z)<<(7-(x%8));
 	   }
 	  }
 	 }
@@ -644,7 +644,7 @@ int main(int argc,char *argv[])
  }
 
  long i;
- for(i=0;i<output_size;i++)
+ for(i=0;i<isTileMap?output_size:unique_tiles*(tile_size*tile_size*depth/8);i++)
  {
   if(targetFormat!=TARGET_NEOGEO_SPR||(targetFormat==TARGET_NEOGEO_SPR&&!((i/2)%2)))	fputc(tiles[i],tilefile1);
   else if(targetFormat==TARGET_NEOGEO_SPR&&((i/2)%2))									fputc(tiles[i],tilefile2);
